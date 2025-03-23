f690711f041f17a5168870167b14998ff285c33e
Sanjeev Balakrishnan <sanjeev.balakrishnan@icloud.com>
2025-03-23 14:54:04 +0100

    fix: explicit repaint not needed if state used properly for rendering

--- components/Field.tsx
+++ components/Field.tsx
@@ -19,8 +19,8 @@ const Field = React.forwardRef<FieldRef, FieldProps>((props, ref) => {
   const [prevCard, setPrevCard] = useState<MemoryCard | null>(null);
   const [isValidationInProgress, setValidationInProgress] = useState(false);
 
-  const repaintField = () => setField([...props.field]);
   const reset = () => window.location.reload()
+
 
   const onCardClick = (row: number, col: number) => {
     if (!firstCardClicked) {
@@ -34,14 +34,12 @@ const Field = React.forwardRef<FieldRef, FieldProps>((props, ref) => {
 
     const currCard = fieldState[row][col];
     currCard.show();
-    repaintField();
 
     if (prevCard) {
       if (prevCard.icon === currCard.icon) {
         prevCard.setMatched(true);
         currCard.setMatched(true);
         setPrevCard(null);
-        repaintField();
         if (fieldState.flat().every((card) => card.isMatched())) {
           setTimeout(() => {
             window.alert("Congrats :) You solved it!");
@@ -54,7 +52,6 @@ const Field = React.forwardRef<FieldRef, FieldProps>((props, ref) => {
           prevCard.hide();
           currCard.hide();
           setPrevCard(null);
-          repaintField();
           setValidationInProgress(false);
         }, 750);
       }
@@ -67,7 +64,7 @@ const Field = React.forwardRef<FieldRef, FieldProps>((props, ref) => {
 
   return (
     <div className={styles.field}>
-      {props.field.map((row, i) => (
+      {fieldState.map((row, i) => (
         <div key={i.toString()} className="inline-flex flex-row justify-space-between gap-4">
           {row.map((card, j) => (
             <Card
